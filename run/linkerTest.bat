13e795f53f95380b95635921a92113ec2f2b2b7e
Andreas Fröhlich <andreas@immerfroehlich.de>
2020-03-31 14:58:01 +0200

    Removed a lot from the old code that is not part of the libdiscid binding.

--- run/linkerTest.bat
+++
@@ -1,8 +0,0 @@
-set LIB_PATH=native
-set CLASSPATH=build
-set CLASS=test.ca.benow.jni.LinkerTest
-#VERBOSE= -verbose:jni -verbose
-
-echo Running: java %VERBOSE% -Djava.library.path=%LIB_PATH% -cp %CLASSPATH% %CLASS% %1 %2 %3 %4
-java %VERBOSE% -Djava.library.path=%LIB_PATH% -cp %CLASSPATH% %CLASS% %1 %2 %3 %4
-
--- src/discid/test/de/immerfroehlich/discid/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author andreas
- *
- */
-package test.de.immerfroehlich.discid;
